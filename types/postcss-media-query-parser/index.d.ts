ec72e2175ba2e9ff82237818088c275bf0815c2f
Dylan Awalt-Conley <web@dylan.ac>
2023-04-06 16:16:48 -0400

    extend postcss-media-query-parser type definitions
    better prediction of how different nodes relate to each other

--- types/postcss-media-query-parser/index.d.ts
+++ types/postcss-media-query-parser/index.d.ts
@@ -9,19 +9,66 @@ declare namespace MediaQuery {
       | 'value' // i.e. '100px'
       | 'media-type' // i.e. 'print'
       | 'keyword' // i.e. 'not'
-    after: string
     before: string
+    after: string
     value: string
     sourceIndex: number
-    parent?: MediaQuery.Node
-    nodes: MediaQuery.Node[]
+    parent?: Node
+    nodes?: Node[]
   }
 
   interface List extends Node {
     type: 'media-query-list'
+    nodes: Query[]
+    parent: never
+  }
+
+  interface Query extends Node {
+    type: 'media-query'
+    nodes: (FeatureExpression | Keyword | Type)[]
+    parent: List
+  }
+
+  interface FeatureExpression extends Node {
+    type: 'media-feature-expression'
+    nodes: [Feature, Colon, Value] | [Node] // singular if feature expression is just a set of parenthesis
+    parent: Query
+  }
+
+  interface Feature extends Node {
+    type: 'media-feature'
+    nodes: never
+    parent: FeatureExpression
+  }
+
+  interface Colon extends Node {
+    type: 'colon'
+    nodes: never
+    parent: FeatureExpression
+    value: ':'
+  }
+
+  interface Value extends Node {
+    type: 'value'
+    nodes: never
+    parent: FeatureExpression
+  }
+
+  interface Type extends Node {
+    type: 'media-type'
+    nodes: never
+    parent: Query
+    // value: 'all' | 'print' | 'screen' | 'tty' | 'tv' | 'projection' | 'handheld' | 'braille' | 'embossed' | 'aural' | 'speech'
+  }
+
+  interface Keyword extends Node {
+    type: 'keyword'
+    nodes: never
+    parent: Query
+    // value: 'and' | 'only' | 'not'
   }
 }
 
 declare module 'postcss-media-query-parser' {
-  export default function parseMedia(queryString: string): MediaQuery.Node
+  export default function parseMedia(queryString: string): MediaQuery.List
 }
