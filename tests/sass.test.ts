b2e838aaa9f9dd8246c4515dd4db3691d47a9ff2
Dylan Awalt-Conley <web@dylan.ac>
2023-11-14 15:59:36 -0500

    test: simplify media query tests with fewer devices

--- tests/sass.test.ts
+++ tests/sass.test.ts
@@ -9,6 +9,26 @@ const defaultConfig: ResponsiveImagesOptions = {
     dryRun: true,
     filenameFormat: (_id, _src, width, format) => `output-${width}.${format}`,
   },
+  devices: [
+    {
+      w: 1920,
+      h: 1200,
+      dppx: [1],
+      flip: false,
+    },
+    {
+      w: 1024,
+      h: 768,
+      dppx: [2, 1],
+      flip: true,
+    },
+    {
+      w: 768,
+      h: 432,
+      dppx: [4, 2.5],
+      flip: true,
+    },
+  ],
 }
 
 const compile = async (
@@ -50,182 +70,38 @@ const compileLegacy = async (
 
 const defaultExpected = scss`
   .bg-image {
-    @media (orientation: landscape) and (min-width: 1921px) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 1920px) and (min-width: 1681px) {
+    @media (orientation: landscape)  and (min-width: 1025px) {
       background-image: url('/img/output-2048.jpeg');
     }
-    @media (orientation: landscape) and (max-width: 1680px) and (min-width: 1441px) {
-      background-image: url('/img/output-2048.jpeg');
+    @media (orientation: landscape) and (max-width: 1024px) and (min-width: 769px) and (min-resolution: 97dpi) {
+      background-image: url("/img/output-2048.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1440px) and (min-width: 1367px) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-3072.jpeg');
+    @media (orientation: landscape) and (max-width: 1024px) and (min-width: 769px) and (max-resolution: 96dpi) {
+      background-image: url("/img/output-1080.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1440px) and (min-width: 1367px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-1442.jpeg');
+    @media (orientation: landscape) and (max-width: 768px) and (min-resolution: 241dpi) {
+      background-image: url("/img/output-3072.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1366px) and (min-width: 1281px) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-3072.jpeg');
+    @media (orientation: landscape) and (max-width: 768px) and (max-resolution: 240dpi) and (min-resolution: 97dpi) {
+      background-image: url("/img/output-2048.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1366px) and (min-width: 1281px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-1442.jpeg');
+    @media (orientation: landscape) and (max-width: 768px) and (max-resolution: 96dpi) {
+      background-image: url("/img/output-1080.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1280px) and (min-width: 1025px) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-3072.jpeg');
+    @media (orientation: portrait) and (min-width: 433px) and (min-resolution: 97dpi) {
+      background-image: url("/img/output-2048.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1280px) and (min-width: 1025px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
+    @media (orientation: portrait) and (min-width: 433px) and (max-resolution: 96dpi) {
+      background-image: url("/img/output-1080.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1280px) and (min-width: 1025px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-1442.jpeg');
+    @media (orientation: portrait) and (max-width: 432px) and (min-resolution: 241dpi) {
+      background-image: url("/img/output-2048.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1024px) and (min-width: 961px) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
+    @media (orientation: portrait) and (max-width: 432px) and (max-resolution: 240dpi) and (min-resolution: 97dpi) {
+      background-image: url("/img/output-1080.jpeg");
     }
-    @media (orientation: landscape) and (max-width: 1024px) and (min-width: 961px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 960px) and (min-width: 769px) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 960px) and (min-width: 769px) and (max-resolution: 192dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 960px) and (min-width: 769px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 768px) and (min-width: 691px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 768px) and (min-width: 691px) and (max-resolution: 288dpi) and (min-resolution: 241dpi) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 768px) and (min-width: 691px) and (max-resolution: 240dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 768px) and (min-width: 691px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 690px) and (min-width: 641px) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 690px) and (min-width: 641px) and (max-resolution: 192dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 690px) and (min-width: 641px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 640px) and (min-width: 481px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-3072.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 640px) and (min-width: 481px) and (max-resolution: 288dpi) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 640px) and (min-width: 481px) and (max-resolution: 192dpi) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 640px) and (min-width: 481px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 640px) and (min-width: 481px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 480px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 480px) and (max-resolution: 288dpi) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 480px) and (max-resolution: 192dpi) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 480px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: landscape) and (max-width: 480px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: portrait) and (min-width: 801px) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: portrait) and (min-width: 801px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 800px) and (min-width: 769px) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 800px) and (min-width: 769px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 800px) and (min-width: 769px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 768px) and (min-width: 601px) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 768px) and (min-width: 601px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 600px) and (min-width: 433px) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 600px) and (min-width: 433px) and (max-resolution: 192dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 600px) and (min-width: 433px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 432px) and (min-width: 413px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-2048.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 432px) and (min-width: 413px) and (max-resolution: 288dpi) and (min-resolution: 241dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 432px) and (min-width: 413px) and (max-resolution: 240dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 432px) and (min-width: 413px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-432.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 412px) and (min-width: 361px) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 412px) and (min-width: 361px) and (max-resolution: 192dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 412px) and (min-width: 361px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-432.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 360px) and (min-width: 321px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 360px) and (min-width: 321px) and (max-resolution: 288dpi) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 360px) and (min-width: 321px) and (max-resolution: 192dpi) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 360px) and (min-width: 321px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 360px) and (min-width: 321px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-432.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 320px) and (min-resolution: 289dpi) {
-      background-image: url('/img/output-1442.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 320px) and (max-resolution: 288dpi) and (min-resolution: 193dpi) {
-      background-image: url('/img/output-960.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 320px) and (max-resolution: 192dpi) and (min-resolution: 145dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 320px) and (max-resolution: 144dpi) and (min-resolution: 97dpi) {
-      background-image: url('/img/output-640.jpeg');
-    }
-    @media (orientation: portrait) and (max-width: 320px) and (max-resolution: 96dpi) {
-      background-image: url('/img/output-432.jpeg');
+    @media (orientation: portrait) and (max-width: 432px) and (max-resolution: 96dpi) {
+      background-image: url("/img/output-432.jpeg");
     }
   }
 `
