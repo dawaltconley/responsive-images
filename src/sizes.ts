36c44ec8d4a44bdfe2b01aa8f673fe8de962fe6b
Dylan Awalt-Conley <web@dylan.ac>
2023-11-27 15:29:27 -0500

    refactor: parser function, using postcss parser for as much as possible

--- src/sizes.ts
+++ src/sizes.ts
@@ -1,9 +1,10 @@
 import type { Image } from './types'
 import type Device from './device'
-import mediaParser from 'postcss-media-query-parser'
+import mediaParser, { MediaQuery, Node } from 'postcss-media-query-parser'
 import UnitValue from './unit-values'
 import QueryMap from './query-map'
 import { filterSizes } from './utilities'
+import findLastIndex from 'lodash/findLastIndex'
 
 export const MediaFeature = [
   'max-width',
@@ -76,67 +77,74 @@ export default class Sizes {
    * @return an array of SizesQuery.Object objects describing media query parameters
    */
   static parse(sizesQueryString: string): SizesQuery[] {
-    return sizesQueryString
-      .split(/\s*,\s*/)
-      .map<SizesQuery>((descriptor: string) => {
-        const conditions: MediaCondition[] = []
-        const parsed = descriptor.match(/^(.*)\s+(\S+)$/) // TODO get this from parser instead; last node in media-query
-        if (!parsed) return { conditions, width: UnitValue.parse(descriptor) }
-
-        const [, mediaCondition, widthString]: string[] = parsed
-        const width = UnitValue.parse(widthString)
-        if (mediaCondition) {
-          // TODO handle expressions wrapped in extra parenthesis
-          const mediaQuery = mediaParser(mediaCondition).nodes[0]
-          for (const node of mediaQuery.nodes) {
-            if (node.type === 'media-feature-expression') {
-              const mediaFeature = node.nodes.find(
-                n => n.type === 'media-feature'
-              )?.value
-              const valueString = node.nodes.find(
-                n => n.type === 'value'
-              )?.value
-              if (mediaFeature && isMediaFeature(mediaFeature) && valueString) {
-                // mediaFeature should always be truthy. value is only falsy with boolean media features, which in our case can be safely ignored.
-                const value = UnitValue.parse(valueString)
-                if (!value.uses('px'))
-                  throw new Error(
-                    `Invalid query unit ${value}: only px is supported`
-                  )
-                conditions.push({ mediaFeature, value })
-              }
-            } else if (node.type === 'keyword') {
-              // } else if (node.type === 'keyword' && node.value === 'and') {
-              // continue // TODO wouldn't be valid sizes attribute, but regardless this doesn't work
-              // // maybe parse with cssValue here?
-
-              if (node.value === 'and' || node.value === 'only') {
-                // ignore; add next valid node to the conditions list
-                continue
-              } else if (node.value === 'not') {
-                // TODO handle not
-                // eslint-disable-next-line no-console
-                console.warn(`not keyword is not yet handled; ignoring`)
-                continue
-              } else {
-                throw new Error(`invalid media query keyword: ${node.value}`)
-              }
-            } else if (node.type === 'media-type') {
-              if (node.value === 'all')
-                // ignore; all is the only valid media-type
-                continue
-              else
-                throw new Error(
-                  `media type ${node.value} cannot be used in a sizes attribute`
-                )
-            } else {
-              // eslint-disable-next-line no-console
-              console.error(node)
-              throw new Error('unhandled node type')
-            }
-          }
-        }
-        return { conditions, width }
-      })
+    return mediaParser(sizesQueryString).nodes.map<SizesQuery>(mediaQuery => {
+      const imageSize = mediaQuery.nodes.splice(
+        findLastIndex(
+          mediaQuery.nodes,
+          node => node.type === 'media-feature-expression'
+        ) + 1
+      )
+      const conditions = parseConditions(mediaQuery)
+      const width = parseImageSize(imageSize)
+      return { conditions, width }
+    })
   }
+}
+
+function parseConditions(query: MediaQuery): MediaCondition[] {
+  const conditions: MediaCondition[] = []
+  for (const node of query.nodes) {
+    if (node.type === 'media-feature-expression') {
+      const mediaFeature = node.nodes.find(
+        n => n.type === 'media-feature'
+      )?.value
+      const valueString = node.nodes.find(n => n.type === 'value')?.value
+      if (mediaFeature && isMediaFeature(mediaFeature) && valueString) {
+        // mediaFeature should always be truthy. value is only falsy with boolean media features, which in our case can be safely ignored.
+        const value = UnitValue.parse(valueString)
+        if (!value.uses('px'))
+          throw new Error(`Invalid query unit ${value}: only px is supported`)
+        conditions.push({ mediaFeature, value })
+      }
+    } else if (node.type === 'keyword') {
+      if (node.value === 'and' || node.value === 'only') {
+        // ignore; add next valid node to the conditions list
+        continue
+      } else if (node.value === 'not') {
+        // TODO handle not
+        // eslint-disable-next-line no-console
+        console.warn(`not keyword is not yet handled; ignoring`)
+        continue
+      } else {
+        throw new Error(`invalid media query keyword: ${node.value}`)
+      }
+    } else if (node.type === 'media-type') {
+      if (node.value === 'all')
+        // ignore; all is the only valid media-type
+        continue
+      else
+        throw new Error(
+          `media type ${node.value} cannot be used in a sizes attribute`
+        )
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(node)
+      throw new Error('unhandled node type')
+    }
+  }
+  return conditions
+}
+
+function parseImageSize(nodes: Node[]): UnitValue {
+  const values = nodes.map(n => n.value)
+  try {
+    if (values.length === 1) return UnitValue.parse(values[0])
+    if (values.length === 2 && values[0] === 'width')
+      return UnitValue.parse(values[1])
+  } catch (e) {
+    console.error(e) // eslint-disable-line no-console
+  }
+
+  const msg = nodes[0]?.parent?.value || values.join(' ')
+  throw new Error(`Unable to parse image sizes from query: ${msg}`)
 }
