408267827b2b472b6b62c60dde169d1867b39036
Dylan Awalt-Conley <web@dylan.ac>
2024-03-07 20:41:32 -0500

    feat: better types for hast output

--- src/index.ts
+++ src/index.ts
@@ -83,7 +83,14 @@ export {
   ResolvedImage,
 } from './lib/device'
 export { default as Image } from './lib/image'
-export { default as Metadata, SizesMetadata, HtmlOptions } from './lib/metadata'
+export {
+  default as Metadata,
+  SizesMetadata,
+  HtmlOptions,
+  HastOutput,
+  HastSource,
+  HastImage,
+} from './lib/metadata'
 export type { ConfiguredImage, ImageOptions } from './lib/image'
 export type {
   default as MediaQueries,
