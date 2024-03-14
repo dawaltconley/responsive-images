76f5fad5738677b341293140f46af1cc41748c28
Dylan Awalt-Conley <web@dylan.ac>
2024-03-14 10:43:12 -0400

    build: simplify build script
    keep output dir in package.json

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-out_dir=$(cat tsconfig.json | jq -r '.compilerOptions.outDir' || echo dist)
+out_dir=$1
+echo "compiling to $out_dir"
 rm -r "$out_dir"
-npx tsc
+npx tsc --outDir "$out_dir"
 [ -d "$out_dir/sass" ] || mkdir "$out_dir/sass"
 cp ./src/sass/_mixins.scss "$out_dir/sass"
 echo done
